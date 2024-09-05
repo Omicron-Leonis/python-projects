c853862a869585dd0cd5f0488820c5848534e125
Omicron-Leonis <intisar.mahiyat@gmail.com>
2024-09-05 20:59:10 +0600

    if

---
+++ Python1.py
@@ -0,0 +1,14 @@
+#Print a Sentence
+print("Welcome to the world of programming!")
+
+#Print a Number
+print(4)
+
+#Next Line after print
+print("Hello Python \n")
+
+#Pass multiple arguemts for printing
+print("hello,S")
+
+#End arguement of Print Statement
+print("Welcome to" , end= "*")
---
+++ varible hw.py
@@ -0,0 +1,20 @@
+print("Storing data in variables")
+#Creating different variables
+x= 1
+y= 2
+z= 3
+v= 4
+w= 5
+print(x)
+print(y)
+print(z)
+print(v)
+print(w)
+
+happybirthday = "happy birthday"
+print(happybirthday)
+
+#User input
+name=input("enter your name: ")
+
+print("happy birthday",name)
